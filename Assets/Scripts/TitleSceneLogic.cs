45b02aa8ff35f6d37fe3242702a46a2dab757976
EliotShu <eliotshu@tencent.com>
2020-05-28 23:06:10 +0800

    增加了标题Scene

---
+++ Assets/Scripts/TitleSceneLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneLogic : MonoBehaviour
+{
+    public AudioSource bgm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm.Play(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
